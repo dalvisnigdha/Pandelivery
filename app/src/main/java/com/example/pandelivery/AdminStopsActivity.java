f843d59d0f9362c01365a5025e2138e5cb6766ff
dalvisnigdha <dalvisnigdha@gmail.com>
2020-05-27 23:24:15 +0530

    except user pages rest implemented

--- app/src/main/java/com/example/pandelivery/AdminStopsActivity.java
+++ app/src/main/java/com/example/pandelivery/AdminStopsActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -68,8 +69,12 @@ public class AdminStopsActivity extends AppCompatActivity {
                 });
 
 // in this block
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_dropdown_item, items);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         dropdown.setAdapter(adapter);
+     
+
+
         signout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick (View view){
