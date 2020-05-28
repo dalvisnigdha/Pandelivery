574047da29031d5b8f7682904098ebee3cf8ecc7
aby03 <abhay.saxena003@gmail.com>
2020-05-29 02:48:12 +0530

    Current Location Added.

--- app/src/main/java/com/example/pandelivery/UserMainActivity.java
+++ app/src/main/java/com/example/pandelivery/UserMainActivity.java
@@ -7,14 +7,17 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.view.Menu;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -26,10 +29,12 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class UserMainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class UserMainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener {
 //    Button signout;
 
     // Map Objects
+    GoogleMap mMap;
     UiSettings mUiSettings;
 
     @Override
@@ -37,10 +42,14 @@ public class UserMainActivity extends AppCompatActivity implements OnMapReadyCal
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_main);
 
+        // Check Permission
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocationPermission();
+        }
+        // Maps
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
 //        signout = findViewById(R.id.signout);
 
 //        signout.setOnClickListener(new View.OnClickListener() {
@@ -54,22 +63,79 @@ public class UserMainActivity extends AppCompatActivity implements OnMapReadyCal
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mUiSettings = googleMap.getUiSettings();
+    public void onMapReady(GoogleMap googleMap) {        // Check Permission
+        mMap = googleMap;
+        // Map Settings
+        if (checkLocationPermission()) {
+            mMap.setMyLocationEnabled(true);
+            mMap.setOnMyLocationButtonClickListener(this);
+            mMap.setOnMyLocationClickListener(this);
+        }
+        // Map UI Settings
+        mUiSettings = mMap.getUiSettings();
         mUiSettings.setMyLocationButtonEnabled(true);
         mUiSettings.setZoomControlsEnabled(true);
-//        mUiSettings.setCompassEnabled(true);
-//        mUiSettings.setScrollGesturesEnabled(true);
+        //        mUiSettings.setCompassEnabled(true);
+        //        mUiSettings.setScrollGesturesEnabled(true);
         mUiSettings.setZoomGesturesEnabled(true);
-//        mUiSettings.setTiltGesturesEnabled(true);
+        //        mUiSettings.setTiltGesturesEnabled(true);
         mUiSettings.setRotateGesturesEnabled(true);
-//        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-//            @Override
-//            public boolean onMarkerClick(Marker marker) {
-////                int position = (int)(marker.getTag());
-////                Toast.makeText(con, ""+marker.getPosition(), Toast.LENGTH_LONG).show();
-//                return false;
-//            }
-//        });
+        //        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+        //            @Override
+        //            public boolean onMarkerClick(Marker marker) {
+        ////                int position = (int)(marker.getTag());
+        ////                Toast.makeText(con, ""+marker.getPosition(), Toast.LENGTH_LONG).show();
+        //                return false;
+        //            }
+        //        });
+
+
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    // Check Location Permissions
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    public boolean checkLocationPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Asking user if explanation is needed
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            mMap.setMyLocationEnabled(true);
+            mMap.setOnMyLocationButtonClickListener(this);
+            mMap.setOnMyLocationClickListener(this);
+            return false;
+        } else {
+            return true;
+        }
     }
 }
