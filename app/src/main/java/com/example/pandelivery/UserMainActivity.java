e48feac991523ae6fe9d1371144afef24e97ef54
aby03 <abhay.saxena003@gmail.com>
2020-05-29 02:07:21 +0530

    Maps Zoom and Scroll. UI fixed too

--- app/src/main/java/com/example/pandelivery/UserMainActivity.java
+++ app/src/main/java/com/example/pandelivery/UserMainActivity.java
@@ -16,17 +16,21 @@ import android.view.View;
 import android.widget.Button;
 import android.view.Menu;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class UserMainActivity extends AppCompatActivity {
-//Button signout;
-//    Location currentLocation;
-//    FusedLocationProviderClient fusedLocationProviderClient;
+
+public class UserMainActivity extends AppCompatActivity implements OnMapReadyCallback {
+//    Button signout;
+
+    // Map Objects
+    UiSettings mUiSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,20 +41,7 @@ public class UserMainActivity extends AppCompatActivity {
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        setContentView(R.layout.activity_maps);
-
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setTitle("User");
-        // Construct a GeoDataClient.
-        Object mGeoDataClient = Places.getGeoDataClient(this, null);
-
-        // Construct a PlaceDetectionClient.
-        Object mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
-
-        // Construct a FusedLocationProviderClient.
-        FusedLocationProviderClient mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 //        signout = findViewById(R.id.signout);
-
 
 //        signout.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -61,74 +52,24 @@ public class UserMainActivity extends AppCompatActivity {
 //
 //        });
     }
-    private void getLocationPermission() {
-        /*
-         * Request location permission, so that we can get the location of the
-         * device. The result of the permission request is handled by a callback,
-         * onRequestPermissionsResult.
-         */
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mLocationPermissionGranted = true;
-        } else {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
-        }
-    }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        boolean mLocationPermissionGranted = false;
-        switch (requestCode) {
-            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    mLocationPermissionGranted = true;
-                }
-            }
-        }
-        updateLocationUI();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        getMenuInflater().inflate(R.menu.main_menu,menu);
-        return true;
-
-    }
-
-    public void onMapReady(GoogleMap map) {
-        GoogleMap mMap = map;
-
-        // Do other setup activities here too, as described elsewhere in this tutorial.
-
-        // Turn on the My Location layer and the related control on the map.
-        updateLocationUI();
-
-        // Get the current location of the device and set the position of the map.
-        getDeviceLocation();
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        int id = item.getItemId();
-        if(id==R.id.signout)
-        {
-            Intent I = new Intent(UserMainActivity.this, MainActivity.class);
-            startActivity(I);
-            return false;
-        }
-        if(id==R.id.List_View)
-        {
-            Intent I = new Intent(UserMainActivity.this, ListViewActivity.class);
-            startActivity(I);
-            return false;
-        }
-        return true;
+    public void onMapReady(GoogleMap googleMap) {
+        mUiSettings = googleMap.getUiSettings();
+        mUiSettings.setMyLocationButtonEnabled(true);
+        mUiSettings.setZoomControlsEnabled(true);
+//        mUiSettings.setCompassEnabled(true);
+//        mUiSettings.setScrollGesturesEnabled(true);
+        mUiSettings.setZoomGesturesEnabled(true);
+//        mUiSettings.setTiltGesturesEnabled(true);
+        mUiSettings.setRotateGesturesEnabled(true);
+//        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+//            @Override
+//            public boolean onMarkerClick(Marker marker) {
+////                int position = (int)(marker.getTag());
+////                Toast.makeText(con, ""+marker.getPosition(), Toast.LENGTH_LONG).show();
+//                return false;
+//            }
+//        });
     }
 }
