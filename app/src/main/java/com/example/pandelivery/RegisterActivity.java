85a66b78afeeb0d92d221b30691bc71810fb5ad6
aby03 <abhay.saxena003@gmail.com>
2020-05-30 01:38:18 +0530

    User update in firestore on registration

--- app/src/main/java/com/example/pandelivery/RegisterActivity.java
+++ app/src/main/java/com/example/pandelivery/RegisterActivity.java
@@ -25,9 +25,6 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
